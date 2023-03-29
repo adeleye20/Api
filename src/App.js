424c6cb33fb7a9cc183255d86b875ba29891ed5c
adeleye20 <adeleyedaniel655@gmail.com>
2023-03-29 20:37:59 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+// import FetchGet from './API/fetchGet';
+import FetchPost from './API/FetchPost';
+
+function App() {
+  return (
+    <div className="App">
+      <FetchPost/>
+    </div>
+  );
+}
+
+export default App;
